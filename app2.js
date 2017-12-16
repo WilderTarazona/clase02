816d1077bf0b9b0071d1d0ce0f64e99bae5bf449
Tarazona <wtarazonac@gmail.com>
2017-12-16 12:16:29 -0500

    app 4

--- app2.js
+++ app2.js
@@ -1,13 +1,24 @@
+// var http = require('http');
+// var puerto = 3000;
+
+// http.createServer(function (req, res) {
+// 	res.writeHead(200);
+// 	res.write('Hola mundo');
+// 	setTimeout(function () {
+// 		res.end();
+// 		console.log("El proceso termino.");
+// 	}, 5000);
+// }).listen(puerto);
+
+// console.log("estamos escuchando en el puerto: ", puerto);
+
 var http = require('http');
-var puerto = 3000;
-
-http.createServer(function (req, res) {
+var puerto = 8080;
+http.createServer(function(req, res) {
 	res.writeHead(200);
-	res.write('Hola mundo');
-	setTimeout(function () {
-		res.end();
-		console.log("El proceso termino.");
-	}, 5000);
-}).listen(puerto);
+	res.write('Mandamos a procesar');
+	setTimeout(function(){
+		console.log('')
+	})
 
-console.log("estamos escuchando en el puerto: ", puerto);
+})
---
+++ app4.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var _ = require('lodash');
+var arr = [1, 2, 3, 4, "Gato"];
+var puerto = 8000;
+http.createServer(function(req, res) {
+	res.writeHead(200);
+	res.write('Saludos a todos');
+	_.each(arr, function(val, i){
+		console.log(val, "texto extra");
+	});
+	res.end();
+}).listen(puerto);
+console.log("Estamos escuchando en el puerto", puerto);
