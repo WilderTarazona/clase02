0287a46be0d7546ff78e0737c9daa9d4c596a944
Tarazona <wtarazonac@gmail.com>
2017-12-16 11:19:57 -0500

    nuevos cambios

--- app1.js
+++ app1.js
@@ -1,9 +1,20 @@
+// var http = require('http');
+// var puerto = 3000;
+// http.createServer(function (req, res) {
+// 	res.writeHead(200);
+// 	res.write("Hola mundo_\n");
+// 	res.end();
+// }).listen(puerto);
+// console.log("estamos escuchando", puerto);
+
 var http = require('http');
 var puerto = 3000;
-http.createServer(function (req, res) {
+var manejador = function (req, res) {
 	res.writeHead(200);
-	res.write("Hola mundo_"\n);
+	console.log("Esta respuesta solo lo ve en el servidor");
+	res.write("Hola mundo_\n");
 	res.end();
-}).listen(puerto);
-
+});
+var servidor = http.createServer(manejador);
+servidor.listen(puerto);
 console.log("estamos escuchando", puerto);
